7b08b98ade7c10f83a9026a00f680f733c59a06b
victor loginov <tkoe88@zoho.com>
2025-05-09 01:50:19 +0200

    added reversible page transitions

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,31 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideZoneChangeDetection } from '@angular/core';
 import {
   provideRouter,
+  Router,
+  ViewTransitionInfo,
   withComponentInputBinding,
   withRouterConfig,
   withViewTransitions,
 } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
 import { routes } from './app.routes';
+
+const transitionConfig = ({ transition }: ViewTransitionInfo) => {
+  const  router = inject(Router);
+  const urlTree = router.parseUrl('/projects');
+  const  config = {
+           paths: 'exact',
+    matrixParams: 'exact',
+        fragment: 'ignored',
+     queryParams: 'ignored',
+  } as const;
+  const wrapper = document.getElementById('router');
+  if (router.isActive(urlTree, config)) {
+    wrapper?.style.setProperty('view-transition-name', 'page-back');
+  } else {
+    wrapper?.style.setProperty('view-transition-name', 'page-forward');
+  }
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,7 +35,8 @@ export const appConfig: ApplicationConfig = {
       withComponentInputBinding(), // *bind URL params to input()
       // *pass params automatically to children of the bound route
       withRouterConfig({ paramsInheritanceStrategy: 'always' }),
-      withViewTransitions() // *allows Page Transitions
+      // *allows Page Transitions
+      withViewTransitions({ onViewTransitionCreated: transitionConfig }),
     ),
     provideAnimationsAsync(), // *AngularAnimations enabled
   ],
