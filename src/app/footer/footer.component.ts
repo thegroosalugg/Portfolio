8c48e4feeea4c1ced52d0463714855320d8460e6
victor loginov <tkoe88@zoho.com>
2025-05-11 03:39:52 +0200

    footer updated

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { USER } from 'app/profile/user.profile';
+import { FontAwesomeService } from 'app/shared/font.awesome.service';
+import { IconService } from 'app/shared/icon.service';
 
 @Component({
      selector: 'app-footer',
-     imports: [],
+      imports: [FontAwesomeModule],
   templateUrl: './footer.component.html',
      styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
   user = USER;
+  private icons = inject(IconService);
+
+  constructor() {
+    inject(FontAwesomeService);
+  }
+
+  angularIcon() {
+    return this.icons.get('Angular');
+  }
 }
