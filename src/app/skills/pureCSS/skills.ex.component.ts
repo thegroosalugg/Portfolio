2bd233eda79cf1ed2df99c6d0029fc01459e5c30
victor loginov <tkoe88@zoho.com>
2025-05-31 01:02:20 +0200

    removed all hover effect from mobile

--- src/app/skills/pureCSS/skills.ex.component.ts
+++ src/app/skills/pureCSS/skills.ex.component.ts
@@ -1,35 +1,35 @@
-import { Component, ElementRef, signal, viewChild } from '@angular/core';
-import { USER } from 'app/profile/user.profile';
-import { ICONS } from '../../shared/icons';
+// import { Component, ElementRef, signal, viewChild } from '@angular/core';
+// import { USER } from 'app/profile/user.profile';
+// import { ICONS } from '../../shared/icons';
 
-// pure CSS height transition.
-// Not as good as Animations - only supported by Chrome/Edge right now
-@Component({
-     selector: 'app-skills-ex',
-      imports: [],
-  templateUrl: './skills.ex.component.html',
-     styleUrl: './skills.ex.component.scss',
-})
-export class SkillsExComponent {
-  user   = USER;
-  icons  = ICONS;
-  isRow  = signal(false);
-  height = signal(328); // initial CSS values
-  list   = viewChild.required<ElementRef>('list');
+// // pure CSS height transition.
+// // Not as good as Animations - only supported by Chrome/Edge right now
+// @Component({
+//      selector: 'app-skills-ex',
+//       imports: [],
+//   templateUrl: './skills.ex.component.html',
+//      styleUrl: './skills.ex.component.scss',
+// })
+// export class SkillsExComponent {
+//   user   = USER;
+//   icons  = ICONS;
+//   isRow  = signal(false);
+//   height = signal(328); // initial CSS values
+//   list   = viewChild.required<ElementRef>('list');
 
-  getSrc(icon: string) {
-    return `/icons/${icon.toLowerCase()}.svg`;
-  }
+//   getSrc(icon: string) {
+//     return `/icons/${icon.toLowerCase()}.svg`;
+//   }
 
-  getIcon(icon: string) {
-    return this.icons[icon as keyof typeof this.icons];
-  }
+//   getIcon(icon: string) {
+//     return this.icons[icon as keyof typeof this.icons];
+//   }
 
-  toggleGrid() {
-    if (!this.isRow()) {
-      const { height } = this.list().nativeElement.getBoundingClientRect();
-      this.height.set(height);
-    }
-    this.isRow.update((v) => !v);
-  }
-}
+//   toggleGrid() {
+//     if (!this.isRow()) {
+//       const { height } = this.list().nativeElement.getBoundingClientRect();
+//       this.height.set(height);
+//     }
+//     this.isRow.update((v) => !v);
+//   }
+// }
